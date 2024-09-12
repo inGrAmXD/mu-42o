510cd5c36c66695eabf12ed0ae236b27ff246b4e
admin <admin@ARNB-0021.local>
2024-09-12 18:27:29 -0300

    added banner

--- client/src/RootLayout.tsx
+++ client/src/RootLayout.tsx
@@ -24,7 +24,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
             <ScrollToHashElement behavior="smooth" block="center" />
             <header className="m-auto max-w-[1328px]">
               <img
-                className="hidden object-cover"
+                className="object-cover"
                 src={headerBg}
               />
             </header>
